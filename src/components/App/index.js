b6ccd7d08ab1422bafd187fc90a68c030ef6e064
Hasan Ilyas <hasanilyas@outlook.com>
2019-02-17 17:09:25 +0500

    Update state of series array

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,9 +3,21 @@ import Intro from '../Intro';
 import './App.css';
 
 class App extends Component {
+  
   state = {
     series: []
   }
+
+  componentDidMount(){
+
+    const series = ["Vikings", "Game of Thrones", "Friends"];
+    
+    setTimeout(() => {
+      this.setState({ series });
+    }, 2000);
+
+  }
+
   render() {
     return (
       <div className="App">
