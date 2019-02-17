40e6541c475ad34b3639081f51e899040dd29125
Hasan Ilyas <hasanilyas@outlook.com>
2019-02-17 13:46:59 +0500

    Create first functional component called 'Intro'

--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//const greetings = React.createElement('h1', {}, "Hello World!");
-
-const getCurrentDate = () => {
-    const date = new Date();
-    return date.toDateString();
-}
-
-const greetings = <h1>Hello World! Current Date: { getCurrentDate() }</h1>
-
-ReactDOM.render(greetings, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
